0f3cc3265b36b9a348c4dec221c94e1a1930502c
ik1025 <ik1025@nyu.edu>
2021-02-06 14:31:14 -0500

    Components Created

---
+++ budget-react/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header({totalIncome}) {
+	return (
+
+		<header>
+			<h1>Income Tracker</h1>
+
+			<div className="totalIncome">
+				${totalIncome}
+			</div>
+
+		</header>
+
+	)
+}
+
+export default Header;
---
+++ budget-react/src/components/incomeForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function IncomeForm() {
+	return(
+
+		<form className="income-form">
+			<div className="form-inner">
+				<input type="text" name="desc" id="desc" placeholder/>
+			</div>
+		</form>
+
+	)
+}
+
+export default IncomeForm;
